ba1056f0ba83759aac7240f450ba6da59d17a950
Luis Alejandro <luisalejandro.coder@gmail.com>
2023-04-27 14:44:39 -0300

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,17 @@
+let name =  document.getElementById("name");
+let title = document.getElementById("title");
+let description = document.getElementById("description")
+let about = document.getElementById("about")
+let extra = document.getElementById("extra")
+let talk = document.getElementById("talk")
+
+name.innerHTML = "Hola! Soy Luis Alejandro Machado Orozco";
+title.innerHTML = "Desarrollador web en formación";
+about.innerHTML = "Sobre Mi"
+description.innerHTML = "Desarrollador web con habilidades en HTML, CSS y JavaScript, con un enfoque en la creación de sitios web interactivos y responsivos. Si bien no tengo experiencia laboral previa, he trabajado en proyectos personales y he realizado cursos en línea para seguir mejorando mis habilidades. Soy una persona responsable y proactiva, con un fuerte enfoque en el trabajo en equipo y la solución de problemas. También disfruto aprendiendo cosas nuevas y siempre estoy buscando maneras de mejorars";
+
+extra.innerHTML = "<h3>Hobby</h3><ul><li>Hacer deporte</li><li>Gaming</li><li></li></ul><h3>Extra Laboral</h3><ul><li>Servicios de logistica y envios</li><li>Mecanica</li></ul>"
+
+talk.innerHTML = "<h3>Hablemos</h3><p>Puedo aplicar mis habilidades en desarrollo web para impulsar tu proyecto o negocio. Como desarrollador web en formación, tengo experiencia en HTML, CSS y JavaScript, y estoy motivado para seguir mejorando mis habilidades y aprender cosas nuevas. Si buscas un colaborador proactivo y enfocado en resultados</p>"
+
+const burger = document.querySelector('.burger')
